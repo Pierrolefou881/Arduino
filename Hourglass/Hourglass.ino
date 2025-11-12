557311ca69eb74c8c1ea44aa939331d4ef32faf7
Pierre Debas <dpierre394@gmail.com>
2025-11-12 21:00:37 +0100

    Tested output connections

--- Hourglass/Hourglass.ino
+++ Hourglass/Hourglass.ino
@@ -4,14 +4,29 @@
 #include <DigitalOutput.hpp>
 #include <DigitalInput.hpp>
 #include <InOutFactory.hpp>
+#include <CallbackFactory.hpp>
 
 Util::Memory::S_ptr<InOut::Digital::DigitalInput> start_button{ };
 Util::Memory::S_ptr<InOut::Digital::DigitalInput> reset_button{ };
+Util::Memory::S_ptr<InOut::Digital::DigitalOutput> red{ };
+Util::Memory::S_ptr<InOut::Digital::DigitalOutput> yellow1{ };
+Util::Memory::S_ptr<InOut::Digital::DigitalOutput> yellow2{ };
+Util::Memory::S_ptr<InOut::Digital::DigitalOutput> green1{ };
+Util::Memory::S_ptr<InOut::Digital::DigitalOutput> green2{ };
+Util::Memory::S_ptr<InOut::Digital::DigitalOutput> blue{ };
 
 void setup() {
   // put your setup code here, to run once:
   start_button = InOut::Factory::InOutFactory::create_digital_input(12);
   reset_button = InOut::Factory::InOutFactory::create_digital_input(11);
+  blue = InOut::Factory::InOutFactory::create_digital_output(2);
+  green1 = InOut::Factory::InOutFactory::create_digital_output(3);
+  green2 = InOut::Factory::InOutFactory::create_digital_output(4);
+  yellow1 = InOut::Factory::InOutFactory::create_digital_output(5);
+  yellow2 = InOut::Factory::InOutFactory::create_digital_output(6);
+  red = InOut::Factory::InOutFactory::create_digital_output(7);
+
+  start_button->StateChanged = Util::Event::CallbackFactory::create_callback(&toggle_leds);
   Serial.begin(9600);
 }
 
@@ -20,4 +35,14 @@ void loop() {
   auto start = start_button->read_value();
   auto reset = reset_button->read_value();
   Serial.println(start + reset);
+}
+
+void toggle_leds(const InOut::InOutBase* sender, int args)
+{
+  red->write_value(args);
+  yellow1->write_value(args);
+  yellow2->write_value(args);
+  green1->write_value(args);
+  green2->write_value(args);
+  blue->write_value(args);
 }
